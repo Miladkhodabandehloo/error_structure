717ae674b46226b59c55b35ea422e0e307994ce3
milad khodabandehloo <engmiladkh1372work@gmail.com>
2019-10-20 14:24:37 +0330

    add authentication protection for sample api.

--- apps/sample/views.py
+++ apps/sample/views.py
@@ -1,12 +1,18 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 from .serializers import SomeSerializer, NoteSerializer
 from best_practice.errors import APIException
 
 
 class SampleApiView(APIView):
+    permission_classes = (IsAuthenticated,)
+
     def post(self, request):
         # raise APIException.authorization()
         serializer = SomeSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         return Response(data=["some_data"])
+
+    def get(self, request):
+        return Response("some thing for get.")
